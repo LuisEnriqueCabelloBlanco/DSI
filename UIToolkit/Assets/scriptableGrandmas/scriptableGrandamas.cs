b1a6de23f11a9bef2225b627735e5736f42355d3
Erisolo <marsolor@ucm.es>
2024-05-08 15:52:33 +0200

    scriptables ig

---
+++ UIToolkit/Assets/scriptableGrandmas/scriptableGrandamas.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/GrandmaScriptableObject", order = 1)]
+public class SpawnManagerScriptableObject : ScriptableObject
+{
+    public string grandmaName;
+    public string fotoPath;
+
+    public int te; //out of 5
+    public int edad; // out of 5
+    public int nietos; //out of 5
+
+    public string consejo;
+}
